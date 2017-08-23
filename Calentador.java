5a16cae3f3b10c105109e38cdd2fb3eb1cd89adb
De La Torre Lara Arturo <arturo.delato@hotmail.com>
2017-08-22 23:25:58 -0500

    Agragar "incrmento = 3" y limites de temperatura

--- Calentador.java
+++ Calentador.java
@@ -1,20 +1,22 @@
 public class Calentador
 {
     private int temperatura;
+    private int incremento;
     
     Calentador()
     {
         temperatura = 15;
+        incremento = 3;
     }
     
     public void calentar()
     {
-        temperatura = temperatura + 5;
+        temperatura = temperatura + incremento;
     }
     
     public void enfriar()
     {
-        temperatura = temperatura - 5;
+        temperatura = temperatura - incremento;
     }
     
     public void temperaturaActual()
