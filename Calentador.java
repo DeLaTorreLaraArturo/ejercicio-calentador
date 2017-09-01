ea5eb0ce03f8aea5f220008ab7fcb67afadbaad3
De La Torre Lara Arturo <arturo.delato@hotmail.com>
2017-08-31 19:31:38 -0500

    Se Agrego documentacion y en el condiconal para no exceder temperaturas maximas o minimas si excede estas se le asigna un valor igual a la minima o maxima

--- Calentador.java
+++ Calentador.java
@@ -1,3 +1,7 @@
+/**
+ * @author Arturo De La Torre Lara
+ * @version 31 de Agosto del 2017
+ */
 public class Calentador
 {
     private int temperatura;
@@ -5,14 +9,17 @@ public class Calentador
     private int mini;
     private int max;
     
-    Calentador(int minimo, int maximo)
+    public Calentador(int minimo, int maximo)
     {
         temperatura = 15;
         incremento = 3;
         mini = minimo;
         max = maximo;
     }
-    
+    /**
+     * Si el incremento en la temperatur se desea mayor o menor al establecido puede modificarse con este metodo
+     * @param inc paramtero para el valor que se desea sea ahora el incremento
+     */
     public void modificarIncremento(int inc)
     {
         if(inc > 0)
@@ -24,7 +31,9 @@ public class Calentador
             incremento = 0;
         }
     }
-    
+    /**
+     * Aumenta le suma el incremento a la temperatura y checa el condicional para que esta no rebace el limite establecido en la maxima temperatura
+     */
     public void calentar()
     {
         if(temperatura+incremento <= max)
@@ -34,9 +43,12 @@ public class Calentador
         else
         {
             System.out.println("La temperatura ya no puede subir mas");
+            temperatura = max;
         }
     }
-    
+    /**
+     *Resta a la temperatura el incremento establecido y checa en el condicional que la temperatura no se menor a la minima establecida 
+     */
     public void enfriar()
     {
         if(temperatura-incremento >= mini)
@@ -46,10 +58,13 @@ public class Calentador
         else
         {
             System.out.println("La temperatura ya no puede bajar mas");
+            temperatura = mini;
         }
         
     }
-    
+    /**
+     * @return Retorna el valor en el que se encuentra actualmente la temperatura
+     */
     public int temperaturaActual()
     {
        return(temperatura);
